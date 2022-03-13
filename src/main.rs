8af774bcd286642fb31e692151242e14e32eca80
Kallu-A <lucas.aries@protonmail.com>
2022-03-13 15:52:37 +0100

    add: spinner + msg to user

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use clap::{arg, ColorChoice, Parser};
-use anyhow::{anyhow, Context, Result};
+use clap::Parser;
+use anyhow::{anyhow, Result};
 use std::io;
 use std::io::Write;
 use std::thread::sleep;
 use std::time::Duration;
+use image::GenericImageView;
 use image::io::Reader as ImageReader;
+use indicatif::{ProgressBar, ProgressStyle};
 
-
+/// Struct of the cli
 #[derive(Parser)]
 struct Cli {
+    /// Path to where looking for the picture
     #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
-
+    /// Path to where save the result
     #[clap(parse(from_os_str))]
     result: std::path::PathBuf,
 }
@@ -37,23 +40,37 @@ fn main() -> Result<()> {
         }
     }
 
-/*
-    let content = std::fs::read_to_string(args.path.clone())
-        .with_context(|| format!("could not read file `{}`", args.path.to_str().unwrap()))?;
-    println!("file content: {}", content);*/
+    handler(args)?;
+    Ok(())
+}
+
+/// Do the processing of the args after all the paths have been correctly tested
+fn handler(args: Cli) -> Result<()> {
+    let pb = indicatif::ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(80));
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.blue} {msg}")
+            .unwrap()
+            .tick_strings(&[
+                "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"
+            ]),
+    );
+    pb.set_message("Calculating...");
 
     let img = ImageReader::open(args.path)?.decode()?;
 
-    let res = img.grayscale();
-    res.save(args.result);
+    //let res = img.grayscale();
+    //res.save(args.result)?;
+    /*
 
-    let pb = indicatif::ProgressBar::new(100);
-    for i in 0..100 {
-        sleep(Duration::new(0, 20000000));
-        pb.println(format!("[+] finished #{}", i));
-        pb.inc(1);
-    }
-    pb.finish_with_message("done");
-    println!("environement setup");
+        for i in 0..100 {
+            sleep(Duration::new(0, 20000000));
+            pb.println(format!("[+] finished #{}", i));
+            pb.inc(1);
+        }
+        */
+
+    sleep(Duration::new(5, 0));
+    pb.finish_with_message("Done ✅");
     Ok(())
 }
