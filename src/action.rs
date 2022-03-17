852fb03c906773a6d9c9b3cb9814a8ca001fc7a2
Kallu-A <lucas.aries@protonmail.com>
2022-03-17 13:21:45 +0100

    add: separate action fn from main.rs to action.rs

---
+++ src/action.rs
@@ -0,0 +1,162 @@
+use crate::{histogram_gray, histogram_rgb, ProgressBarCustom};
+use anyhow::{anyhow, Context};
+use image::imageops::FilterType;
+use image::DynamicImage;
+use std::io;
+use std::io::Write;
+
+/// Function who take a input
+pub fn take_input(message: &str) -> String {
+    let mut input = String::new();
+    println!("{}", message);
+
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
+/// ask for a filter and return the filter
+fn filter_ask_resize() -> Result<FilterType, anyhow::Error> {
+    let input = take_input("Filter: \n`1` = speed:fast, quality:low \n`2` = speed:medium quality:medium \n`3` = speed:slow quality:high");
+    let filter_u8 = input
+        .parse::<u8>()
+        .with_context(|| format!("invalid filter value, {}", input))?;
+
+    match filter_u8 {
+        1 => Ok(FilterType::Nearest),
+        2 => Ok(FilterType::CatmullRom),
+        3 => Ok(FilterType::Gaussian),
+        _ => Err(anyhow!("invalid filter value")),
+    }
+}
+
+// All the subcommands actions below
+
+/// Do the blur action
+pub fn blur(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    let input = take_input("How much you want it blur: `[0.0; 40.0]`");
+    pb.launch();
+    Ok(img.blur(
+        input
+            .parse::<f32>()
+            .with_context(|| format!("invalid sigma value, {}", input))?,
+    ))
+}
+
+/// Do the resize action
+pub fn resize(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    let input = take_input("new dimension : `width/height`");
+    let input_vec = input.split('/').collect::<Vec<&str>>();
+
+    if input_vec.len() != 2 {
+        return Err(anyhow!("invalid arguments `width/height` got `{}`", input));
+    }
+
+    let nwidth = input_vec[0]
+        .parse::<u32>()
+        .with_context(|| format!("invalid format value, {}", input))?;
+    let nheight = input_vec[1]
+        .parse::<u32>()
+        .with_context(|| format!("invalid format value, {}", input))?;
+
+    let filter = filter_ask_resize()?;
+    pb.launch();
+    Ok(img.resize_exact(nwidth, nheight, filter))
+}
+
+/// Do the resize_ratio action
+pub fn resize_ratio(
+    img: DynamicImage,
+    pb: &ProgressBarCustom,
+) -> Result<DynamicImage, anyhow::Error> {
+    let input = take_input("new dimension : `width/height`");
+    let input_vec = input.split('/').collect::<Vec<&str>>();
+
+    if input_vec.len() != 2 {
+        return Err(anyhow!("invalid arguments `width/height` got `{}`", input));
+    }
+
+    let nwidth = input_vec[0]
+        .parse::<u32>()
+        .with_context(|| format!("invalid format value, {}", input))?;
+    let nheight = input_vec[1]
+        .parse::<u32>()
+        .with_context(|| format!("invalid format value, {}", input))?;
+
+    let filter = filter_ask_resize()?;
+    pb.launch();
+    Ok(img.resize(nwidth, nheight, filter))
+}
+
+/// Do the greyscale action
+pub fn grayscale(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    pb.launch();
+    Ok(img.grayscale())
+}
+
+/// Do the contrast action
+pub fn constrast(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    let input = take_input("Adjust the contrast: `[-80.0; 200.0]`");
+    pb.launch();
+    Ok(img.adjust_contrast(
+        input
+            .parse::<f32>()
+            .with_context(|| format!("invalid value, {}", input))?,
+    ))
+}
+
+/// Do the brighten action
+pub fn brighten(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    let input = take_input("Adjust the brightness: `[-255; 255]`");
+    pb.launch();
+    Ok(img.brighten(
+        input
+            .parse::<i32>()
+            .with_context(|| format!("invalid value, {}", input))?,
+    ))
+}
+
+/// Do the rotate90 action
+pub fn rotate90(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    pb.launch();
+    Ok(img.rotate90())
+}
+
+/// Do the rotate180 action
+pub fn rotate180(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    pb.launch();
+    Ok(img.rotate180())
+}
+
+/// Do the rotate270 action
+pub fn rotate270(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    pb.launch();
+    Ok(img.rotate270())
+}
+
+/// Do the flipv action
+pub fn flipv(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    pb.launch();
+    Ok(img.flipv())
+}
+
+/// Do the fliph action
+pub fn fliph(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    pb.launch();
+    Ok(img.fliph())
+}
+
+pub fn histogram(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
+    let input = take_input("Only gray color , or RGB ? `G/RGB`").to_uppercase();
+    let res = if input == "RGB" {
+        histogram_rgb(img)
+    } else if input == "G" {
+        histogram_gray(img)
+    } else {
+        return Err(anyhow!("Wrong arguments"));
+    }
+    .unwrap();
+    pb.launch();
+
+    Ok(res)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
+mod action;
 mod histogram;
 mod progress_bar_custom;
 
 use std::env::current_dir;
-use std::io;
-use std::io::Write;
 
+use crate::action::take_input;
 use crate::histogram::{histogram_gray, histogram_rgb};
 use crate::progress_bar_custom::progresse_bar_custom::ProgressBarCustom;
 use crate::Action::{
     Blur, Brighten, Contrast, Fliph, Flipv, Grayscale, Histogram, Resize, Resizeratio, Rotate180,
     Rotate270, Rotate90,
 };
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, Result};
 use clap::{Parser, Subcommand};
-use image::imageops::FilterType;
 use image::io::Reader as ImageReader;
-use image::DynamicImage;
 
-/// Found a issue ? go here : "https://github.com/Kallu-A/image-utility"
 #[derive(Parser)]
 #[clap(name = "image-utility")]
-#[clap(about = "Does some basic operation on an image", long_about = None)]
+#[clap(about = "Does some basic operation on an image \n Found a a issue ? go here: 'https://github.com/Kallu-A/image-utility'", long_about = None)]
 #[clap(version = "1.0", author = "Kallu. <lucas.aries@protonmail.com>")]
 struct Cli {
     /// Path to the picture you want to transform
@@ -61,16 +58,6 @@ enum Action {
     Fliph,
     /// Create the histogram of the image, `gray` parameter does the average of the RGB, `rgb` do 3 curves for each color
     Histogram,
-}
-
-/// Function who take a input
-fn take_input(message: &str) -> String {
-    let mut input = String::new();
-    println!("{}", message);
-
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut input).unwrap();
-    input.trim().to_string()
 }
 
 fn main() -> Result<()> {
@@ -117,188 +104,21 @@ fn action_do(args: &Cli) -> Result<()> {
     let img = ImageReader::open(&args.path)?.decode()?;
 
     let res = match args.action {
-        Blur => blur_action(img, &pb)?,
-        Resize => resize_action(img, &pb)?,
-        Resizeratio => resize_ratio_action(img, &pb)?,
-        Grayscale => grayscale_action(img, &pb)?,
-        Contrast => constrast_action(img, &pb)?,
-        Brighten => brighten_action(img, &pb)?,
-        Rotate90 => rotate90_action(img, &pb)?,
-        Rotate180 => rotate180_action(img, &pb)?,
-        Rotate270 => rotate270_action(img, &pb)?,
-        Flipv => flipv_action(img, &pb)?,
-        Fliph => fliph_action(img, &pb)?,
-        Histogram => histogram_action(img, &pb)?,
+        Blur => action::blur(img, &pb)?,
+        Resize => action::resize(img, &pb)?,
+        Resizeratio => action::resize_ratio(img, &pb)?,
+        Grayscale => action::grayscale(img, &pb)?,
+        Contrast => action::constrast(img, &pb)?,
+        Brighten => action::brighten(img, &pb)?,
+        Rotate90 => action::rotate90(img, &pb)?,
+        Rotate180 => action::rotate180(img, &pb)?,
+        Rotate270 => action::rotate270(img, &pb)?,
+        Flipv => action::flipv(img, &pb)?,
+        Fliph => action::fliph(img, &pb)?,
+        Histogram => action::histogram(img, &pb)?,
     };
 
     res.save(&args.result)?;
     pb.done();
     Ok(())
-}
-
-// All the subcommands actions below
-
-/// Do the blur action
-fn blur_action(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
-    let input = take_input("How much you want it blur: `[0.0; 40.0]`");
-    pb.launch();
-    Ok(img.blur(
-        input
-            .parse::<f32>()
-            .with_context(|| format!("invalid sigma value, {}", input))?,
-    ))
-}
-
-/// Do the resize action
-fn resize_action(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
-    let input = take_input("new dimension : `width/height`");
-    let input_vec = input.split('/').collect::<Vec<&str>>();
-
-    if input_vec.len() != 2 {
-        return Err(anyhow!("invalid arguments `width/height` got `{}`", input));
-    }
-
-    let nwidth = input_vec[0]
-        .parse::<u32>()
-        .with_context(|| format!("invalid format value, {}", input))?;
-    let nheight = input_vec[1]
-        .parse::<u32>()
-        .with_context(|| format!("invalid format value, {}", input))?;
-
-    let filter = filter_ask()?;
-    pb.launch();
-    Ok(img.resize_exact(nwidth, nheight, filter))
-}
-
-/// Do the resize_ratio action
-fn resize_ratio_action(
-    img: DynamicImage,
-    pb: &ProgressBarCustom,
-) -> Result<DynamicImage, anyhow::Error> {
-    let input = take_input("new dimension : `width/height`");
-    let input_vec = input.split('/').collect::<Vec<&str>>();
-
-    if input_vec.len() != 2 {
-        return Err(anyhow!("invalid arguments `width/height` got `{}`", input));
-    }
-
-    let nwidth = input_vec[0]
-        .parse::<u32>()
-        .with_context(|| format!("invalid format value, {}", input))?;
-    let nheight = input_vec[1]
-        .parse::<u32>()
-        .with_context(|| format!("invalid format value, {}", input))?;
-
-    let filter = filter_ask()?;
-    pb.launch();
-    Ok(img.resize(nwidth, nheight, filter))
-}
-
-/// Do the greyscale action
-fn grayscale_action(
-    img: DynamicImage,
-    pb: &ProgressBarCustom,
-) -> Result<DynamicImage, anyhow::Error> {
-    pb.launch();
-    Ok(img.grayscale())
-}
-
-/// Do the contrast action
-fn constrast_action(
-    img: DynamicImage,
-    pb: &ProgressBarCustom,
-) -> Result<DynamicImage, anyhow::Error> {
-    let input = take_input("Adjust the contrast: `[-80.0; 200.0]`");
-    pb.launch();
-    Ok(img.adjust_contrast(
-        input
-            .parse::<f32>()
-            .with_context(|| format!("invalid value, {}", input))?,
-    ))
-}
-
-/// Do the brighten action
-fn brighten_action(
-    img: DynamicImage,
-    pb: &ProgressBarCustom,
-) -> Result<DynamicImage, anyhow::Error> {
-    let input = take_input("Adjust the brightness: `[-255; 255]`");
-    pb.launch();
-    Ok(img.brighten(
-        input
-            .parse::<i32>()
-            .with_context(|| format!("invalid value, {}", input))?,
-    ))
-}
-
-/// Do the rotate90 action
-fn rotate90_action(
-    img: DynamicImage,
-    pb: &ProgressBarCustom,
-) -> Result<DynamicImage, anyhow::Error> {
-    pb.launch();
-    Ok(img.rotate90())
-}
-
-/// Do the rotate180 action
-fn rotate180_action(
-    img: DynamicImage,
-    pb: &ProgressBarCustom,
-) -> Result<DynamicImage, anyhow::Error> {
-    pb.launch();
-    Ok(img.rotate180())
-}
-
-/// Do the rotate270 action
-fn rotate270_action(
-    img: DynamicImage,
-    pb: &ProgressBarCustom,
-) -> Result<DynamicImage, anyhow::Error> {
-    pb.launch();
-    Ok(img.rotate270())
-}
-
-/// Do the flipv action
-fn flipv_action(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
-    pb.launch();
-    Ok(img.flipv())
-}
-
-/// Do the fliph action
-fn fliph_action(img: DynamicImage, pb: &ProgressBarCustom) -> Result<DynamicImage, anyhow::Error> {
-    pb.launch();
-    Ok(img.fliph())
-}
-
-fn histogram_action(
-    img: DynamicImage,
-    pb: &ProgressBarCustom,
-) -> Result<DynamicImage, anyhow::Error> {
-    let input = take_input("Only gray color , or RGB ? `G/RGB`").to_uppercase();
-    let res = if input == "RGB" {
-        histogram_rgb(img)
-    } else if input == "G" {
-        histogram_gray(img)
-    } else {
-        return Err(anyhow!("Wrong arguments"));
-    }
-    .unwrap();
-    pb.launch();
-
-    Ok(res)
-}
-
-/// ask for a filter and return the filter
-fn filter_ask() -> Result<FilterType, anyhow::Error> {
-    let input = take_input("Filter: \n`1` = speed:fast, quality:low \n`2` = speed:medium quality:medium \n`3` = speed:slow quality:high");
-    let filter_u8 = input
-        .parse::<u8>()
-        .with_context(|| format!("invalid filter value, {}", input))?;
-
-    match filter_u8 {
-        1 => Ok(FilterType::Nearest),
-        2 => Ok(FilterType::CatmullRom),
-        3 => Ok(FilterType::Gaussian),
-        _ => Err(anyhow!("invalid filter value")),
-    }
 }
